2b5a44f1cfb526a1609b2288ce1f0f0721618b36
Joel Simon <joelsimon6@gmail.com>
2022-08-05 17:31:47 -0700

    add team URLS

--- src/routes/Team.svelte
+++ src/routes/Team.svelte
@@ -1,22 +1,27 @@
 <script>
     const team = [
-        'nia',
-        'faith',
-        'joel',
-        'jim',
-        'jeff',
-        'thomas',
-        'ian',
-        'gabe',
-        'tal',
-        'max',
-        'victor'
+        ['nia', null],
+        ['faith', null],
+        ['joel', 'https://www.joelsimon.net/'],
+        ['jim', null],
+        ['jeff', null],
+        ['thomas', 'https://photon.garden/'],
+        ['evan', 'http://www.evcasey.com'],
+        ['ian', 'https://iandoesallthethings.com'],
+        ['gabe'],
+        ['tal'],
+        ['max', 'https://maxbittker.com/'],
+        ['victor']
     ].sort()
 </script>
 <h2>Team</h2>
 <div class="flex flex-wrap flex-col morpho-body-text">
     <!-- <p>{team.join(' ')}</p> -->
-    {#each team as member}
-        <p class='capitalize '>{member}  </p>
+    {#each team as [member, href]}
+        {#if href == null}
+            <p class='capitalize'>{member}  </p>
+        {:else} 
+            <a class='capitalize' {href} target="_blank">{member} ↗</a>
+        {/if}
     {/each}
 </div>

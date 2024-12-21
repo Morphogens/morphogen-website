997d5b8f9077c516f8bb181e2663c4bb566bc63e
joel simon <joel@morphogen.io>
2024-12-21 11:48:30 +0100

    add missing sculpting with words pdf

--- src/routes/Research.svelte
+++ src/routes/Research.svelte
@@ -1,12 +1,19 @@
 <!-- <main class="text-center">
     <div> -->
 <h2>Research</h2>
-<h3>Sculpting with Words</h3>
+<h3>
+  <a href="/research/sculpting_with_words.pdf" target="_blank">
+    Sculpting with Words
+  </a>
+</h3>
 <p class="morpho-body-text text-left">
-    An interactive 3D tool with guidable optimization using signed distance fields and CLIP. Accepted to NeurIPS Creativity WorkShop 2021. 
+  An interactive 3D tool with guidable optimization using signed distance fields
+  and CLIP. Accepted to NeurIPS Creativity WorkShop 2021.
 </p>
-
-<h3>Prosepainter</h3>
+<a href="https://github.com/Morphogens/ProsePainter" target="_blank">
+  <h3>Prosepainter</h3>
+</a>
 <p class="morpho-body-text text-left">
-    An interactive tool to “paint with words.” It incorporates guidable text-to-image generation into a traditional digital painting interface.
+  An interactive tool to “paint with words.” It incorporates guidable
+  text-to-image generation into a traditional digital painting interface.
 </p>
